29807e45c60278bc1b1706882301a431381a9d32
mbilalazam <124085891+mbilalazam@users.noreply.github.com>
2025-10-10 21:10:36 +0000

    Deploying to gh-pages from @ mbilalazam/mbilalazam.github.io@47e65ef26ff3e6505b0f65dcd9e44eae5e71888f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -540,6 +540,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=WfHM6RIAAAAJ", "_blank");
         },
       },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/teckyonAI", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
